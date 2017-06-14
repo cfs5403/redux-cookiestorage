934c12220a8abaf5782a727aaf9c02bc6f5407d1
fschai89 <fschai89@users.noreply.github.com>
2017-06-14 11:46:57 +0800

    Create persistState.js

--- src/persistState.js
+++ src/persistState.js
@@ -4,17 +4,17 @@ import cookies from 'universal-cookie';
 
 /**
  * @description
- * persistState is a Store Enhancer that syncs (a subset of) store state to localStorage.
+ * persistState is a Store Enhancer that syncs (a subset of) store state to cookie.
  *
- * @param {String|String[]} [paths] Specify keys to sync with localStorage, if left undefined the whole store is persisted
+ * @param {String|String[]} [paths] Specify keys to sync with cookie, if left undefined the whole store is persisted
  * @param {Object} [config] Optional config object
- * @param {String} [config.key="redux"] String used as localStorage key
+ * @param {String} [config.key="redux"] String used as cookie key
  * @param {Function} [config.slicer] (paths) => (state) => subset. A function that returns a subset
- * of store state that should be persisted to localStorage
+ * of store state that should be persisted to cookie
  * @param {Function} [config.serialize=JSON.stringify] (subset) => serializedData. Called just before persisting to
- * localStorage. Should transform the subset into a format that can be stored.
+ * cookie. Should transform the subset into a format that can be stored.
  * @param {Function} [config.deserialize=JSON.parse] (persistedData) => subset. Called directly after retrieving
- * persistedState from localStorage. Should transform the data into the format expected by your application
+ * persistedState from cookie. Should transform the data into the format expected by your application
  *
  * @return {Function} An enhanced store
  */
@@ -49,7 +49,7 @@ export default function persistState(paths, config) {
       persistedState = deserialize(cookies.get(key))
       finalInitialState = merge(initialState, persistedState)
     } catch (e) {
-      console.warn('Failed to retrieve initialize state from localStorage:', e)
+      console.warn('Failed to retrieve initialize state from cookie:', e)
     }
 
     const store = next(reducer, finalInitialState, enhancer)
@@ -62,7 +62,7 @@ export default function persistState(paths, config) {
       try {
         cookies.set(key, serialize(subset), { path: '/' });
       } catch (e) {
-        console.warn('Unable to persist state to localStorage:', e)
+        console.warn('Unable to persist state to cookie:', e)
       }
     })
 
